82ce47ca8d6f4e06f87e01604e1f5b8dac183e45
Phemelo Mogane <101391794+pcmCodes@users.noreply.github.com>
2023-08-13 13:57:13 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,41 @@
+// Create web server
+// npm install express --save
+// npm install body-parser --save
+// npm install mongoose --save
+// npm install jsonwebtoken --save
+// npm install passport --save
+// npm install passport-jwt --save
+// npm install bcryptjs --save
+// npm install validator --save
+// npm install gravatar --save
+// npm install nodemon --save-dev
+// npm install concurrently --save-dev
+// npm install -g nodemon
+// npm install -g concurrently
+// npm install --save-dev jest
+// npm install --save-dev supertest
+// npm install --save-dev expect
+// npm install --save-dev jest-cli
+// npm install --save-dev babel-jest babel-preset-es2015 babel-preset-stage-0 babel-preset-react
+// npm install --save-dev babel-cli
+// npm install --save-dev babel-cli babel-preset-env
+// npm install --save-dev babel-cli babel-preset-react babel-preset-stage-0
+// npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0
+// npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0 babel-preset-react
+// npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0 babel-preset-react babel-plugin-transform-class-properties
+// npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0 babel-preset-react babel-plugin-transform-class-properties babel-plugin-transform-object-rest-spread
+// npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0 babel-preset-react babel-plugin-transform-class-properties babel-plugin-transform-object-rest-spread babel-core babel-loader
+// npm install --save-dev babel-cli babel-preset-env babel-preset-stage-0 babel-preset-react babel-plugin-transform-class-properties babel-plugin-transform-object-rest-spread babel-core babel-loader
+// npm run server
+// npm run server:dev
+// npm run server:test
+// npm run server:dev:test
+// npm run test
+// npm run test:watch
+// npm run test:watch:coverage
+// npm run test:watch:coverage:open
+// npm run test:watch:coverage:report
+// npm run test:watch:coverage:report:open
+// npm run test:watch:coverage:report:open:html
+// npm run test:watch:coverage:report:open:text
+// npm run
